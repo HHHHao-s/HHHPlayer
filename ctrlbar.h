0f99ec7d212d1f7a2411c01cdc7d3b05ed841f49
曾荣豪 <2755942193@qq.com>
2024-02-03 11:51:13 +0800

    ctrlBar

---
+++ ctrlbar.h
@@ -0,0 +1,22 @@
+#ifndef CTRLBAR_H
+#define CTRLBAR_H
+
+#include <QWidget>
+
+namespace Ui {
+class CtrlBar;
+}
+
+class CtrlBar : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit CtrlBar(QWidget *parent = nullptr);
+    ~CtrlBar();
+
+private:
+    Ui::CtrlBar *ui;
+};
+
+#endif // CTRLBAR_H
